7edbffb55144538fe8cd6c5b2e57d3f4c08e192d
Aarushi <anu@Aarushis-MacBook-Pro.local>
2025-12-10 16:22:57 +0530

    Query and insert :

--- tsdb/lib/storage/tsdb.ml
+++ tsdb/lib/storage/tsdb.ml
@@ -17,13 +17,20 @@ let time_since (time : float) =
   in time_as_float
 
 
+let overlaps start (en_d:float)=
+  (match (Timedesc.of_timestamp_float_s (Float.add start  7200.)),
+         (Timedesc.of_timestamp_float_s start) ,
+         (Timedesc.of_timestamp_float_s en_d) with
+    | Some t, Some t1,Some  t2 ->
+         (Timedesc.Timestamp.lt (Timedesc.to_timestamp_single t2) (Timedesc.to_timestamp_single t))
+          || (Timedesc.Timestamp.gt (Timedesc.to_timestamp_single t)
+                                    (Timedesc.to_timestamp_single t1))
+    | _,_,  _ ->  failwith "Duration error" )
 
 let time_series() =
 
         let  start = Float.mul 1000.  (Unix.gettimeofday()) in
-
         {
-
             start_time = start;
 
             points = Datapoint_vector.create();
@@ -31,7 +38,7 @@ let time_series() =
             compressed_data = Compressed_data_vector.create();
 
             compressed_size  = 0L;
-          }
+        }
 
 let new_time_series k =
 
@@ -40,17 +47,47 @@ let new_time_series k =
             active_block  = time_series();
             closed_blocks = Timeseries_block_vector.create();
          }
+
+let get_points timeseries start e_nd =
+
+   let overlaps tstamp  =
+    (match (Timedesc.of_timestamp_float_s tstamp),
+           (Timedesc.of_timestamp_float_s start),
+           (Timedesc.of_timestamp_float_s e_nd) with
+    | Some t, Some t1,Some  t2 ->
+        (Timedesc.Timestamp.ge (Timedesc.to_timestamp_single t) (Timedesc.to_timestamp_single t1))
+        &&
+        (Timedesc.Timestamp.le (Timedesc.to_timestamp_single t) (Timedesc.to_timestamp_single t2))
+    | _,_, _ ->  failwith "Duration error" )
+    in
+    CCVector.filter ( fun dp -> overlaps dp.tstamp ) timeseries
+
 let  insert time_s timestamp value =
       let dp = { tstamp = timestamp; value = value } in
       match (time_since timestamp) with
-      | (h, ns) -> if h > 2 || (h = 2 && ns > 0 ) then(
-                     let () = CCVector.push time_s.closed_blocks time_s.active_block in
-                     time_s.active_block <- time_series();
-                     CCVector.push time_s.active_block.points dp
+      (* I should use the time API to check this. *)
+      | (h, ns) -> if h > 2 || (h = 2 && ns > 0 ) then( (*TODO  I check nanoseconds only!*)
+                     let () = CCVector.push time_s.closed_blocks time_s.active_block in time_s.active_block <- time_series();
+                         CCVector.push time_s.active_block.points dp
                      )
                      else(
-                     CCVector.push time_s.active_block.points dp
+                         CCVector.push time_s.active_block.points dp
                      )
+
+
+let query start en_d blocks =
+        let  results = CCVector.create() in
+
+        CCVector.iter(fun block ->
+            if overlaps block.start_time en_d then
+                CCVector.push results (get_points block.points start en_d)
+            else ()
+        )blocks.closed_blocks;
+        if overlaps blocks.active_block.start_time  en_d then
+            CCVector.push results (get_points blocks.active_block.points
+                            start en_d);
+
+        results
 
 end
 
